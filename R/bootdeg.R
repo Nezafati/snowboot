6f0455761f05258a1a431bcbd544c5a4ceaa3a1f
Kusha Nezafati <kusha.nezafati@gmail.com>
2017-09-17 09:54:54 -0500

    Modify documentation for bootdeg. Remove method argument from bootdeg function.

--- R/bootdeg.R
+++ R/bootdeg.R
@@ -4,7 +4,7 @@
 #' based on a LSMI sample. Default is one bootstrap replication.
 #'
 #' @param sam.out A list that is the output of \code{\link{Oempdegreedistrib}}.
-#' @param num.sam A vector of integers containing the numeric ids of the LSMI
+#' @param num.sam A vector of integers containing the numeric IDs of the LSMI
 #'    samples when \code{sam.out$num.sam} is greater than one. When \code{num.sam} is an
 #'    integer, N, LSMI from 1 to N are taken from the input \code{sam.out}.
 #' @param n.boot A positive integer number, the number of bootstrap replications.
@@ -18,26 +18,18 @@
 #'    \item{values}{A list of length \code{num.sam} where each element is a
 #'          vector containing the unique degree values sampled in each LSMI.}
 #'    \item{empd}{A list of length \code{num.sam} where each element contains an
-#'          estimate of degree distribution for each LSMI. The method of
-#'          estimation is set with the \code{method} parameter. If
-#'          \code{method="w"} the object empd.w.p0s is returned, which is
-#'          a weighted bootstrap with a proportion of isolated nodes, p0,
-#'          being estimated by simple random sampling of bootstrapped
-#'          seeds; If \code{method="nw"} the object empd.nw.p0sEkb, which
-#'          is - non-weighted bootstrap with a proportion of isolated nodes,
-#'          p0, being estimated by simple random sampling of bootstrapped seeds
-#'          (see Thompson et al. for details).}
+#'          estimate of degree distribution for each LSMI.}
 #'    \item{num.sam}{Numeric indices corresponding to LSMI samples used for bootstrap.}
 #'    \item{n.boot}{The same object as input argument \code{n.boot}.}
 #'    \item{n.neigh}{The number of waves carried out by the snowball sample.
 #'          This is the same value from \code{sam.out$n.neigh}.}
-#'    \item{seeds1}{A matrix of dimension \code{lenght(num.sam)} x \code{n.seeds} with
-#'          the numeric seed ids. Each row corresponds to one LSMI. The rows are
-#'          present in the same order as the ids in \code{num.sam}.}
+#'    \item{seeds1}{A matrix of dimension \code{lenght(num.sam)} \eqn{\times} \code{n.seeds} with
+#'          the numeric seed IDs. Each row corresponds to one LSMI. The rows are
+#'          present in the same order as the IDs in \code{num.sam}.}
 #'    \item{nodes_of_LSMI}{A list of length \code{length(num.sam)} where each
-#'          element is vector containing the numeric ids of the nodes sampled
+#'          element is vector containing the numeric IDs of the nodes sampled
 #'          using the respective LSMI. The elements are present in the same
-#'          order as the ids in \code{num.sam}.
+#'          order as the IDs in \code{num.sam}.
 #'          Note: nodes_of_LSMI is unreported when n.neigh equals zero.}
 #' @export
 #' @examples
